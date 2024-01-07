a7b6d7a4eb16e693c4419e57a217ebb91fc9d914
nsx_69 <mrmanafest16@gmail.com>
2024-01-07 13:13:39 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,90 @@
+// 'use strict'
+// alert('Hello world')
+
+// alert('Hello')
+
+// let a = 3
+// a = 4
+// alert(a)
+
+// let a = 1
+// let b = 2
+// let c = 3
+// alert(a)
+// alert(b)
+// alert(c)
+
+// let a = 1,
+// 	b = 2,
+// 	c = 3
+
+// let a, b, c
+// a = 1
+// b = 2
+// c = 3
+
+// let a
+// a = 10
+// alert(a)
+// a = 20
+// alert(a)
+
+// let a = 1 + 2 + 3
+// alert(a)
+
+// let a = 8 - 5
+// alert(a)
+
+// let a = 4 * 6
+// alert(a)
+
+// let a = 8 / 4
+// alert(a)
+
+// let a = 10
+// let b = 2
+// alert(a + b)
+
+// let c = 10
+// let d = 5
+// let result = c + d
+// alert(result)
+
+// let a = 10
+// let b = 5
+// let c = a - b
+// let d = 7
+// let result = c + d
+// alert(result)
+
+// let a = 5 + 5 * 3
+// alert(a)
+
+// let a = (8 * 4) / 2 / 2
+// alert(a)
+
+// let a = 2 * (2 + 4 * (3 + 1))
+// alert(a)
+
+// let a = 1.5
+// let b = 0.75
+// let result = a + b
+// alert(result)
+
+// let a = -5
+// let b = 8
+// let result = a * b
+// alert(result)
+
+// let a = 13
+// let b = 5
+// let result = 13 % 5
+// alert(result)
+
+// let a = 2;
+// let b = 10;
+// let result = 2 ** 10;
+// alert(result);
+
+// let a = 3 * 2 ** 3 * 3;
+// alert(a);
